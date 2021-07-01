c23f47019501b7c84cd0a6b6ecefa2935c5029d1
AlxDfy <ajduffay@gmail.com>
2021-07-01 14:40:44 -0700

    Add Folder of WAV capabilities
    Minor addition of being able to take a folder of WAV files, output .txt transcriptions to a folder: 'transcripts' + username

--- transcribe.py
+++ transcribe.py
@@ -20,3 +20,18 @@ def transcribe(sound_file):
     transcription = tokenizer.batch_decode(predicted_ids)[0]
 
     return transcription
+
+
+# transcribe WAV files into folders for each user within a folder: "transcripts"
+def transcribe_folder(target_folder):
+    # go through all WAV files
+    for file in os.listdir(target_folder):
+        if file.endswith(".wav"):
+            # get the username, output to named folder
+            # naming convention - ID2C00_voice_tmp_[guid].wav
+            username = str(file).split("_voice_")[0]
+            filename = str(target_folder) +"\\"+ "transcripts" + "\\" + username + "\\" + str(file)[:-3]+'txt'
+            # output to user folder with same name .txt
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+            with open(filename, "w") as f:
+                f.write(transcribe(str(target_folder)+file))
